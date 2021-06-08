a76fda878341290790c3b65fea51859495f4b944
Marco Lapolla <marco.lapolla5@gmail.com>
2021-06-08 10:24:26 +0200

    Fix spaces python

--- zed_camera/camera_node.py
+++ zed_camera/camera_node.py
@@ -24,119 +24,119 @@ import pyzed.sl as sl
 
 # Class definition of the calibration function
 class ZedNode(Node):
-	def __init__(self):
-		super().__init__("zed_camera_node")
-		self.get_logger().info("ZED camera node is awake...")
+    def __init__(self):
+        super().__init__("zed_camera_node")
+        self.get_logger().info("ZED camera node is awake...")
 
-		self.init = sl.InitParameters()
-		self.init.camera_resolution = sl.RESOLUTION.HD720
-		self.init.depth_mode = sl.DEPTH_MODE.NONE
-		self.cam = sl.Camera()
-		self.status = self.cam.open(self.init)
-		if self.status != sl.ERROR_CODE.SUCCESS:
-		    print(repr(self.status))
-		    sys.exit(1)
+        self.init = sl.InitParameters()
+        self.init.camera_resolution = sl.RESOLUTION.HD720
+        self.init.depth_mode = sl.DEPTH_MODE.NONE
+        self.cam = sl.Camera()
+        self.status = self.cam.open(self.init)
+        if self.status != sl.ERROR_CODE.SUCCESS:
+            print(repr(self.status))
+            sys.exit(1)
 
-		self.runtime = sl.RuntimeParameters()
+        self.runtime = sl.RuntimeParameters()
 
-		self.stream = sl.StreamingParameters()
-		self.stream.codec = sl.STREAMING_CODEC.H264
-		self.stream.bitrate = 4000
-		self.status = self.cam.enable_streaming(self.stream)
-		if self.status != sl.ERROR_CODE.SUCCESS:
-		    print(repr(self.status))
-		    sys.exit(1)
+        self.stream = sl.StreamingParameters()
+        self.stream.codec = sl.STREAMING_CODEC.H264
+        self.stream.bitrate = 4000
+        self.status = self.cam.enable_streaming(self.stream)
+        if self.status != sl.ERROR_CODE.SUCCESS:
+            print(repr(self.status))
+            sys.exit(1)
 
-		self.mat = sl.Mat()
+        self.mat = sl.Mat()
 
-		self.frame = None
-		self.bridge = CvBridge()
+        self.frame = None
+        self.bridge = CvBridge()
 
-		# Acquisition thread
-		self.thread1 = threading.Thread(target=self.get_frame, daemon=True)
-		self.thread1.start()
+        # Acquisition thread
+        self.thread1 = threading.Thread(target=self.get_frame, daemon=True)
+        self.thread1.start()
 
-		# Publishers
-		self.frame_pub = self.create_publisher(Image, "/zed_camera/raw_frame", 10)
-		self.timer = self.create_timer(0.03, self.publish_frame)
+        # Publishers
+        self.frame_pub = self.create_publisher(Image, "/zed_camera/raw_frame", 10)
+        self.timer = self.create_timer(0.03, self.publish_frame)
 
-		self.br = tf2_ros.TransformBroadcaster(self)
-
-		
-		self.static_broadcaster = tf2_ros.StaticTransformBroadcaster(self)
-
-		static_transformStamped = geometry_msgs.msg.TransformStamped()
-
-		static_transformStamped.header.stamp = self.get_clock().now().to_msg()
-		static_transformStamped.header.frame_id = "Rover_CoM"
-		static_transformStamped.child_frame_id = "ZED_Camera_Base"
-
-		static_transformStamped.transform.translation.x =  0.0
-		static_transformStamped.transform.translation.y = 0.0
-		static_transformStamped.transform.translation.z = 0.0
-
-		rot = R.from_euler('zyx', [0.0, 0.0, 0.0], degrees=True)
-		quat = rot.as_quat()
-		static_transformStamped.transform.rotation.x = quat[0]
-		static_transformStamped.transform.rotation.y = quat[1]
-		static_transformStamped.transform.rotation.z = quat[2]
-		static_transformStamped.transform.rotation.w = quat[3]
-
-		self.static_broadcaster.sendTransform(static_transformStamped)
+        self.br = tf2_ros.TransformBroadcaster(self)
 
 
-	# This function save the current frame in a class attribute
-	def get_frame(self):
-		
-		err = self.cam.grab(self.runtime)
+        self.static_broadcaster = tf2_ros.StaticTransformBroadcaster(self)
+
+        static_transformStamped = geometry_msgs.msg.TransformStamped()
+
+        static_transformStamped.header.stamp = self.get_clock().now().to_msg()
+        static_transformStamped.header.frame_id = "Rover_CoM"
+        static_transformStamped.child_frame_id = "ZED_Camera_Base"
+
+        static_transformStamped.transform.translation.x =  0.0
+        static_transformStamped.transform.translation.y = 0.0
+        static_transformStamped.transform.translation.z = 0.0
+
+        rot = R.from_euler('zyx', [0.0, 0.0, 0.0], degrees=True)
+        quat = rot.as_quat()
+        static_transformStamped.transform.rotation.x = quat[0]
+        static_transformStamped.transform.rotation.y = quat[1]
+        static_transformStamped.transform.rotation.z = quat[2]
+        static_transformStamped.transform.rotation.w = quat[3]
+
+        self.static_broadcaster.sendTransform(static_transformStamped)
+
+
+    # This function save the current frame in a class attribute
+    def get_frame(self):
+
+        err = self.cam.grab(self.runtime)
         if (err == sl.ERROR_CODE.SUCCESS) :
             self.cam.retrieve_image(self.mat, sl.VIEW.LEFT)
             self.frame = self.mat.get_data()
 
-				
-	# This function stops/enable the acquisition stream
-	def exit(self):
-		self.start_acquisition = False
-		self.cam.disable_streaming()
-    	self.cam.close()
-		self.thread1.join()
+
+    # This function stops/enable the acquisition stream
+    def exit(self):
+        self.start_acquisition = False
+        self.cam.disable_streaming()
+        self.cam.close()
+        self.thread1.join()
 
 
 
-	# Publisher function
-	def publish_frame(self):
-		
-		if len(self.frame is None or self.frame) == 0:
-			return
+    # Publisher function
+    def publish_frame(self):
 
-		self.image_message = self.bridge.cv2_to_imgmsg(self.frame, encoding="mono8")
-		self.image_message.header = Header()
-		self.image_message.header.stamp = self.get_clock().now().to_msg()
-		self.image_message.header.frame_id = "ZED_Camera_Base"
-		self.frame_pub.publish(self.image_message)
+        if len(self.frame is None or self.frame) == 0:
+            return
+
+        self.image_message = self.bridge.cv2_to_imgmsg(self.frame, encoding="mono8")
+        self.image_message.header = Header()
+        self.image_message.header.stamp = self.get_clock().now().to_msg()
+        self.image_message.header.frame_id = "ZED_Camera_Base"
+        self.frame_pub.publish(self.image_message)
 
 
 
 # Main loop function
 def main(args=None):
 
-	rclpy.init(args=args)
-	node = ZedNode()
-	try:
-		rclpy.spin(node)
-	except KeyboardInterrupt:
-		print('server stopped cleanly')
-		node.exit()
-	except BaseException:
-		print('exception in server:', file=sys.stderr)
-		raise
-	finally:
-		# Destroy the node explicitly
-		# (optional - Done automatically when node is garbage collected)
-		node.destroy_node()
-		rclpy.shutdown() 
+    rclpy.init(args=args)
+    node = ZedNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print('server stopped cleanly')
+        node.exit()
+    except BaseException:
+        print('exception in server:', file=sys.stderr)
+        raise
+    finally:
+        # Destroy the node explicitly
+        # (optional - Done automatically when node is garbage collected)
+        node.destroy_node()
+        rclpy.shutdown()
 
 
 # Main
 if __name__ == '__main__':
-	main()
+    main()
--- zed_camera/slam.py
+++ zed_camera/slam.py
@@ -13,47 +13,47 @@ import pyzed.sl as sl
 
 # Class definition of the calibration function
 class SLAM_Zed_Node(Node):
-	def __init__(self):
-		super().__init__("slam_zed_node")
-		self.get_logger().info("Calibration node is awake...")
+    def __init__(self):
+        super().__init__("slam_zed_node")
+        self.get_logger().info("Calibration node is awake...")
 
-		# Class attributes
-		self.bridge = CvBridge()
-		self.frame = None
+        # Class attributes
+        self.bridge = CvBridge()
+        self.frame = None
 
-		self.init_params = sl.InitParameters(camera_resolution=sl.RESOLUTION.HD720,
-		                     coordinate_units=sl.UNIT.METER,
-		                     coordinate_system=sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP)
+        self.init_params = sl.InitParameters(camera_resolution=sl.RESOLUTION.HD720,
+                             coordinate_units=sl.UNIT.METER,
+                             coordinate_system=sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP)
          
-		self.zed = sl.Camera()
-		status = self.zed.open(self.init_params)
-		if status != sl.ERROR_CODE.SUCCESS:
-		    print(repr(status))
-		    sys.exit(1)
+        self.zed = sl.Camera()
+        status = self.zed.open(self.init_params)
+        if status != sl.ERROR_CODE.SUCCESS:
+            print(repr(status))
+            sys.exit(1)
 
-		self.tracking_params = sl.PositionalTrackingParameters()
-		self.zed.enable_positional_tracking(self.tracking_params)
+        self.tracking_params = sl.PositionalTrackingParameters()
+        self.zed.enable_positional_tracking(self.tracking_params)
 
-		self.runtime = sl.RuntimeParameters()
-		self.camera_pose = sl.Pose()
+        self.runtime = sl.RuntimeParameters()
+        self.camera_pose = sl.Pose()
 
-		self.camera_info = self.zed.get_camera_information()
+        self.camera_info = self.zed.get_camera_information()
 
-		self.py_translation = sl.Translation()
-		self.pose_data = sl.Transform()
+        self.py_translation = sl.Translation()
+        self.pose_data = sl.Transform()
 
-		# Acquisition thread
-		self.thread1 = threading.Thread(target=self.get_pose, daemon=True)
-		self.thread1.start()
+        # Acquisition thread
+        self.thread1 = threading.Thread(target=self.get_pose, daemon=True)
+        self.thread1.start()
 
-		# Publishers
-		self.pose_pub = self.create_publisher(Pose, "/zed_camera/pose", 10)
+        # Publishers
+        self.pose_pub = self.create_publisher(Pose, "/zed_camera/pose", 10)
 
 
-	# This function save the current frame in a class attribute
-	def get_pose(self):
-		
-		if self.zed.grab(runtime) == sl.ERROR_CODE.SUCCESS:
+    # This function save the current frame in a class attribute
+    def get_pose(self):
+
+        if self.zed.grab(runtime) == sl.ERROR_CODE.SUCCESS:
             tracking_state = self.zed.get_position(self.camera_pose)
             if tracking_state == sl.POSITIONAL_TRACKING_STATE.OK:
                 self.rotation = camera_pose.get_rotation_vector()
@@ -62,71 +62,71 @@ class SLAM_Zed_Node(Node):
 
                 msg = PoseStamped()
 
-				msg.header = Header()
-				msg.header.stamp = self.get_clock().now().to_msg()
-				msg.header.frame_id = "zed_cam"
+                msg.header = Header()
+                msg.header.stamp = self.get_clock().now().to_msg()
+                msg.header.frame_id = "zed_cam"
 
-				# Translation
-				msg.pose.position.x = self.translation[0]
-				msg.pose.position.y = self.translation[1]
-				msg.pose.position.z = self.translation[2]
+                # Translation
+                msg.pose.position.x = self.translation[0]
+                msg.pose.position.y = self.translation[1]
+                msg.pose.position.z = self.translation[2]
 
-				rot = R.from_rotvec([self.rotation[0], self.rotation[1], self.rotation[2]])
-				quat = rot.as_quat()
+                rot = R.from_rotvec([self.rotation[0], self.rotation[1], self.rotation[2]])
+                quat = rot.as_quat()
 
-				# short-Rodrigues (angle-axis)
-				msg.pose.orientation.x = quat[0]
-				msg.pose.orientation.y = quat[1]
-				msg.pose.orientation.z = quat[2]
-				msg.pose.orientation.w = quat[3]
+                # short-Rodrigues (angle-axis)
+                msg.pose.orientation.x = quat[0]
+                msg.pose.orientation.y = quat[1]
+                msg.pose.orientation.z = quat[2]
+                msg.pose.orientation.w = quat[3]
 
-				# Publish the message
-				self.pose_pub.publish(msg)
+                # Publish the message
+                self.pose_pub.publish(msg)
 
-				t = geometry_msgs.msg.TransformStamped()
+                t = geometry_msgs.msg.TransformStamped()
 
-				t.header.stamp = self.get_clock().now().to_msg()
-				t.header.frame_id = "world"
-				t.child_frame_id = "Rover_CoM"
-				t.transform.translation.x = msg.pose.position.x
-				t.transform.translation.y = msg.pose.position.y
-				t.transform.translation.z = msg.pose.position.z
-				t.transform.rotation.x = quat[0]
-				t.transform.rotation.y = quat[1]
-				t.transform.rotation.z = quat[2]
-				t.transform.rotation.w = quat[3]
+                t.header.stamp = self.get_clock().now().to_msg()
+                t.header.frame_id = "world"
+                t.child_frame_id = "Rover_CoM"
+                t.transform.translation.x = msg.pose.position.x
+                t.transform.translation.y = msg.pose.position.y
+                t.transform.translation.z = msg.pose.position.z
+                t.transform.rotation.x = quat[0]
+                t.transform.rotation.y = quat[1]
+                t.transform.rotation.z = quat[2]
+                t.transform.rotation.w = quat[3]
 
-				self.br.sendTransform(t)
+                self.br.sendTransform(t)
 
 
-				
-	# This function stops/enable the acquisition stream
-	def exit(self):
-		self.zed.close()
-		self.thread1.join()
 
-	
+    # This function stops/enable the acquisition stream
+    def exit(self):
+        self.zed.close()
+        self.thread1.join()
+
+
 
 # Main loop function
 def main(args=None):
 
-	rclpy.init(args=args)
-	node = SLAM_Zed_Node()
-	try:
-		rclpy.spin(node)
-	except KeyboardInterrupt:
-		print('server stopped cleanly')
-		node.exit()
-	except BaseException:
-		print('exception in server:', file=sys.stderr)
-		raise
-	finally:
-		# Destroy the node explicitly
-		# (optional - Done automatically when node is garbage collected)
-		node.destroy_node()
-		rclpy.shutdown() 
+    rclpy.init(args=args)
+    node = SLAM_Zed_Node()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print('server stopped cleanly')
+        node.exit()
+    except BaseException:
+        print('exception in server:', file=sys.stderr)
+        raise
+    finally:
+        # Destroy the node explicitly
+        # (optional - Done automatically when node is garbage collected)
+        node.destroy_node()
+        rclpy.shutdown()
 
 
 # Main
 if __name__ == '__main__':
-	main()
+    main()
