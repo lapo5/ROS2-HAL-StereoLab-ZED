452f8068c47a93ac1777c10879e0bed67621680e
Marco Lapolla <marco.lapolla5@gmail.com>
2021-07-16 15:16:46 +0200

    Merge

--- zed_camera/slam.py
+++ zed_camera/slam.py
@@ -14,6 +14,7 @@ import threading
 import sys
 import pyzed.sl as sl
 import time
+import tf2_ros
 from scipy.spatial.transform import Rotation as R
 
 # Class definition of the calibration function
