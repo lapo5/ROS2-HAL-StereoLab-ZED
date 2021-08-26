024a8ea80ac70304657cdb8dd6e087431ab2bf15
marcolapo5 <marco.lapolla5@gmail.com>
2021-08-26 13:00:04 +0200

    Fixes

--- zed_camera/slam.py
+++ zed_camera/slam.py
@@ -82,7 +82,7 @@ class SLAM_Zed_Node(Node):
         self.thread1 = threading.Thread(target=self.get_pose, daemon=True)
         self.thread1.start()
 
-    	# Service: stop acquisition
+        # Service: stop acquisition
         self.stop_service = self.create_service(Empty, "/zed_camera/stop_slam", self.stop_slam)
 
 
