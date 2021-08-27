fd6b81a3e02c4046397d4fa60211b2fe2a58ab1a
Marco Lapolla <marco.lapolla5@gmail.com>
2021-08-27 15:10:34 +0200

    Update user

--- start_slam_remotely.sh
+++ start_slam_remotely.sh
@@ -2,7 +2,7 @@
 
 SCRIPT="source /opt/ros/eloquent/setup.bash; source /home/roxy-jetson/ros2_ws/install/setup.bash; ros2 run zed_camera slam"
 HOST="192.168.1.40"
-USERNAME="roxy-jetson"
+USERNAME="roxy"
 PASSWORD="roxy"
 SCR=${SCRIPT/PASSWORD/${PASSWORD}}
 sshpass -p ${PASSWORD} ssh -l ${USERNAME} ${HOST} "${SCRIPT}"
--- start_video_feed_remotely.sh
+++ start_video_feed_remotely.sh
@@ -2,7 +2,7 @@
 
 SCRIPT="source /opt/ros/eloquent/setup.bash; source /home/roxy-jetson/ros2_ws/install/setup.bash; ros2 run zed_camera camera_node"
 HOST="192.168.1.40"
-USERNAME="roxy-jetson"
+USERNAME="roxy"
 PASSWORD="roxy"
 SCR=${SCRIPT/PASSWORD/${PASSWORD}}
 sshpass -p ${PASSWORD} ssh -l ${USERNAME} ${HOST} "${SCRIPT}"
